22304b1b84d451048fa224531eca4e82608e057e
moskvichyov <moskvichyov.p@gmail.com>
2022-04-24 00:10:35 +0300

    24.04.2022

--- app.py
+++ app.py
@@ -1,6 +1,8 @@
 import os
+import re
+from typing import Iterator
 
-from flask import Flask, request
+from flask import Flask, request, Response
 from werkzeug.exceptions import BadRequest
 
 app = Flask(__name__)
@@ -9,47 +11,59 @@ BASE_DIR = os.path.dirname(os.path.abspath(__file__))
 DATA_DIR = os.path.join(BASE_DIR, "data")
 
 
-def build_query(f, query):
-    query_items = query.split('|')
-    result = map(lambda v: v.strip(), f)
-    for item in query_items:
-        split_item = item.split(':')
-        cmd = split_item[0]
-        if cmd == 'filter':
-            arg = split_item[1]
-            result = filter(lambda v, txt=arg: txt in v, result)
-        elif cmd == 'map':
-            arg = int(split_item[1])
-            result = map(lambda v, idx=arg: v.split(" ")[idx], result)
-        elif cmd == 'unique':
-            result = set(result)
-        elif cmd == 'sort':
-            arg = split_item[1]
-            if arg == 'desc':
-                reverse = True
-            else:
-                reverse = False
-            result = sorted(result, reverse=reverse)
-        elif cmd == 'limit':
-            arg = int(split_item[1])
-            result = list(result)[:arg]
-        return result
+def slice_limit(it: Iterator, limit: int) -> Iterator:
+    i=0
+    for item in it:
+        if i < limit:
+            yield item
+        else:
+            break
+        i += 1
 
 
-@app.route("/perform_query")
-def perform_query():
+def apply_cmd(it: Iterator, cmd: str, value: str) -> Iterator:
+    if cmd == "filter":
+        return filter(lambda v: value in v, it)
+    if cmd == "map":
+        idx = int(value)
+        return map(lambda v: v.split(" ")[idx], it)
+    if cmd == "unique":
+        return iter(set(it))
+    if cmd == "sort":
+        reverse = value == "desc"
+        return iter(sorted(it, reverse=reverse))
+    if cmd == "limit":
+        arg = int(value)
+        return slice_limit(it, arg)
+    if cmd == "regex":
+        regex = re.compile(value)
+        return filter(lambda v: regex.search(v), it)
+    return it
+
+
+def build_query(it: Iterator, cmd1: str, value1: str, cmd2: str, value2: str) -> Iterator:
+    result: Iterator = map(lambda v: v.strip(), it)
+    result = apply_cmd(result, cmd1, value1)
+    return apply_cmd(result, cmd2, value2)
+
+
+@app.post("/perform_query")
+def perform_query() -> Response:
     try:
-        query = request.args['query']
-        file_name = request.args['file_name']
+        file_name = request.form['file_name']
+        cmd1 = request.form['cmd1']
+        value1 = request.form['value1']
+        cmd2 = request.form['cmd2']
+        value2 = request.form['value2']
     except KeyError:
-        raise BadRequest(description="Не указаны необходимые параметры")
+        raise BadRequest
 
     file_path = os.path.join(DATA_DIR, file_name)
     if not os.path.exists(file_path):
-        return BadRequest(description=f"{file_name} файл не найден")
+        raise BadRequest(description=f"{file_name} файл не найден")
 
     with open(file_path) as f:
-        result = build_query(f, query)
+        result = build_query(f, cmd1, value1, cmd2, value2 )
         content = '\n'.join(result)
 
     return app.response_class(content, content_type="text/plain")
